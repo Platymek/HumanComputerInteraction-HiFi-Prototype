e3d0fa497a78d8a126fcec59dffe0f645d044ff6
Platymek <hectordavid.selby@gmail.com>
2023-11-22 19:06:08 +0000

    HyperLinkText created, new colour added. README updated with documentation. Need to update all others to allow alt colours after merge.

---
+++ pages/templates/components/HyperLinkText.js
@@ -0,0 +1,65 @@
+import { Pressable } from 'react-native';
+import { Text, View, Button, StyleSheet, Image } from 'react-native';
+
+/*
+    - HyperLinkText Component -
+
+    props:
+    - colors: color palette of page
+    - text: text in big button
+    - link: hyperlink text
+    - (optional) alt: boolean, colour as red instead of green
+*/
+
+export default function HyperLinkText(props) {
+
+    return (
+
+        <View style={{
+
+            padding: 4,
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+        }}>
+            <Text
+
+                style={{
+
+                    color: props.colors[2],
+                    fontWeight: 'bold',
+                    fontSize: 16,
+                }}
+            >
+                {props.text}
+            </Text>
+
+            <Pressable
+
+                onPress={props.onPress}
+                unstable_pressDelay={1}
+
+                style={({ pressed }) => [{
+
+                    color: props.colors[0],
+                    borderRadius: 4,
+                    alignItems: 'center',
+                }]}
+            >
+                <Text
+
+                    style={{
+
+                        fontSize: 16,
+                        fontWeight: 'bold',
+
+                        color: props.alt == true
+                            ? props.colors[5]
+                            : props.colors[3],
+                    }}
+                >
+                    {props.link}
+                </Text>
+            </Pressable>
+        </View>
+    );
+}
